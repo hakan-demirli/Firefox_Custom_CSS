cfc205606da27ec181ce467bc41e65a2a30e22e6
hakan-demirli <emre.demirli@metu.edu.tr>
2023-08-29 23:57:16 +0300

    add user.js

---
+++ user.js
@@ -0,0 +1,13 @@
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+
+user_pref("browser.sessionstore.restore_pinned_tabs_on_demand", true);
+
+user_pref("browser.compactmode.show", true);
+
+user_pref("browser.toolbars.bookmarks.visibility", "never");
+
+user_pref("browser.uidensity", 1);
+
+user_pref("browser.download.autohideButton", false);
+
+
